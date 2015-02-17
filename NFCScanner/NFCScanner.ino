164b1f49263147d5c7c7659c26ad97154550a147
UJamil <usmanajamil@gmail.com>
2015-02-16 21:20:39 -0800

    NFCScanner v0.2
    Write Section functional

--- NFCScanner/NFCScanner.ino
+++ NFCScanner/NFCScanner.ino
@@ -37,18 +37,90 @@ void setup() {
   Serial.begin(9600);	// Initialize serial communications with the PC
   SPI.begin();			// Init SPI bus
   mfrc522.PCD_Init();	// Init MFRC522 card
+
+  // Prepare the key (used both as key A and as key B)
+  // using FFFFFFFFFFFFh which is the default at chip delivery from the factory
+  for (byte i = 0; i < 6; i++) {
+    key.keyByte[i] = 0xFF;
+  }
+
   Serial.println("Scan PICC to see UID and type...");
 }
 
 void loop() {
 
-  byte buffer[34];  
-  byte block;
-  byte status, len;
+  // In this sample we use the second sector,
+  // that is: sector #1, covering block #4 up to and including block #7
+  byte sector         = 1;
+  byte block          = 4;
+  byte dataBlock[]    = {
+    0x01, 0x02, 0x03, 0x04, //  1,  2,   3,  4,
+    0x05, 0x06, 0x07, 0x08, //  5,  6,   7,  8,
+    0x08, 0x09, 0xff, 0x0b, //  9, 10, 255, 12,
+    0x0c, 0x0d, 0x0e, 0x0f  // 13, 14,  15, 16
+  };
+  byte trailerBlock   = 7;
+  byte status;
+  byte buffer[18];
+  byte len;
+  byte size = sizeof(buffer);
 
-  // Prepare key - all keys are set to FFFFFFFFFFFFh at chip delivery from the factory.
-  MFRC522::MIFARE_Key key;
-  for (byte i = 0; i < 6; i++) key.keyByte[i] = 0xFF;
+  if (!mfrc522.PICC_IsNewCardPresent()){
+
+  }
+
+  // Authenticate using key A
+  //  Serial.println("Authenticating using key A...");
+  //  status = mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, trailerBlock, &key, &(mfrc522.uid));
+  //  if (status != MFRC522::STATUS_OK) {
+  //    Serial.print("PCD_Authenticate() failed: ");
+  //    Serial.println(mfrc522.GetStatusCodeName(status));
+  //    return;
+  //  }
+  //
+  //  // Show the whole sector as it currently is
+  //  Serial.println("Current data in sector:");
+  //  mfrc522.PICC_DumpMifareClassicSectorToSerial(&(mfrc522.uid), &key, sector);
+  //  Serial.println();
+  //
+  //  // Read data from the block
+  //  Serial.print("Reading data from block "); 
+  //  Serial.print(block);
+  //  Serial.println(" ...");
+  //  status = mfrc522.MIFARE_Read(block, buffer, &size);
+  //  if (status != MFRC522::STATUS_OK) {
+  //    Serial.print("MIFARE_Read() failed: ");
+  //    Serial.println(mfrc522.GetStatusCodeName(status));
+  //  }
+  //  Serial.print("Data in block "); 
+  //  Serial.print(block); 
+  //  Serial.println(":");
+  //  dump_byte_array(buffer, 16); 
+  //  Serial.println();
+  //  Serial.println();
+  //
+  //  // Authenticate using key B
+  //  Serial.println("Authenticating again using key B...");
+  //  status = mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_B, trailerBlock, &key, &(mfrc522.uid));
+  //  if (status != MFRC522::STATUS_OK) {
+  //    Serial.print("PCD_Authenticate() failed: ");
+  //    Serial.println(mfrc522.GetStatusCodeName(status));
+  //    return;
+  //  }
+  //
+  //
+  //  // Dump the sector data
+  //  Serial.println("Current data in sector:");
+  //  mfrc522.PICC_DumpMifareClassicSectorToSerial(&(mfrc522.uid), &key, sector);
+  //  Serial.println();
+  //
+  //  // Halt PICC
+  //  mfrc522.PICC_HaltA();
+  //  // Stop encryption on PCD
+  //  mfrc522.PCD_StopCrypto1();
+  //
+
+  //WRITEWRITEWRITEWRITEWRITEWRITEWRITEWRITEWRITEWRITEWRITEWRITEWRITEWRITEWRITEWRITEWRITEWRITEWRITEWRITEWRITEWRITEWRITEWRITEWRITE
 
   // Look for new cards
   if ( mfrc522.PICC_IsNewCardPresent() == true) {
@@ -109,6 +181,15 @@ void loop() {
   //  mfrc522.PICC_DumpToSerial(&(mfrc522.uid));
 }
 
+/**
+ * Helper routine to dump a byte array as hex values to Serial.
+ */
+void dump_byte_array(byte *buffer, byte bufferSize) {
+  for (byte i = 0; i < bufferSize; i++) {
+    Serial.print(buffer[i] < 0x10 ? " 0" : " ");
+    Serial.print(buffer[i], HEX);
+  }
+}
 
 
 
